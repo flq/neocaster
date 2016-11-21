c29700bc972d2fa27af3501b3c881ad80a1acb1c
Frank Quednau <fquednau@gmail.com>
2016-11-21 22:25:01 +0100

    Initial commit, getting the test rig going

---
+++ src/lib/NeoMapping.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Neo4j.Driver.V1;
+
+namespace NeoCaster
+{
+    public static class NeoMapping
+    {
+        public static IEnumerable<T> Run<T>(this IStatementRunner runner, string cypher, object parameters) 
+        {
+            var result = runner.Run(cypher, parameters.ToDictionary());
+            return null;
+        }
+    }
+}
