33bcb6830c9bcffc3cd80c7bfa9281674fe4e3dd
Frank Quednau <fquednau@gmail.com>
2017-09-19 08:42:43 +0200

    Documentation on some testing infrastructure

--- NeoCaster.Tests/DryRun/StatementResultStorage.cs
+++ NeoCaster.Tests/DryRun/StatementResultStorage.cs
@@ -5,10 +5,20 @@ using Newtonsoft.Json.Linq;
 
 namespace NeoCaster.Tests.DryRun
 {
+    /// <summary>
+    /// Construct an <see cref="IStatementResult"/> instance
+    /// (implemented by <see cref="DryStatementResult"/>) based on an incoming stream that is written to
+    /// by using the exposed TextWriter at <see cref="Sink"/>.
+     /// </summary>
     public class StatementResultStorage
     {
         private MemoryStream _memoryStream;
 
+        /// <summary>
+        /// Use this to write into the stream that is used to produce the statement result when calling
+        /// <see cref="ProduceDryResult"/>. This stream expects a valid json array.
+        /// You can obtain one by calling <see cref="RenderStatementResultExtension.Render"/>
+        /// </summary>
         public TextWriter Sink
         {
             get
@@ -18,6 +28,12 @@ namespace NeoCaster.Tests.DryRun
             }
         }
 
+        /// <summary>
+        /// Build a <see cref="IStatementResult"/> instance
+        /// (implemented by <see cref="DryStatementResult"/>) from the provided data entered through the
+        /// <see cref="Sink"/>
+        /// </summary>
+        /// <returns>A <see cref="DryStatementResult"/> instance</returns>
         public IStatementResult ProduceDryResult()
         {
             return new DryStatementResult(DryStatementResultSkeleton);
